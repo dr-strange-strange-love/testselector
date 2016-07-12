eecbc0989b64f5517122ce9c7750cc28057bacb6
dr-strange-strange-love <vlafedoseevdis@gmail.com>
2016-07-13 00:01:46 +0300

    functionality disabled

--- jobconstructor/__init__.py
+++ jobconstructor/__init__.py
@@ -3,18 +3,8 @@
 !!!
 It is ADVISABLE not to use/import this (jobconstructor) package in external modules (intended usage is for testing).
 Use/import src package instead (rename it to your liking and remove __main__.py module).
-For safety reasons jobconstructor provides interface to src.
+For safety reasons functionality disabled.
 !!!
 """
 
-from .src import JobConstructor as JC
-'''
-одного этого импорта будет достаточно, оборачивать не нужно
-'''
-
-class JobConstructor():
-	def __init__(self, rootpath):
-		jc = JC(self.rootpath)
-
-	def get_job_by_title(self, title):
-		return jc.get_job_by_title(title)
+from .src import JobConstructor
