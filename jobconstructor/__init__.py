61ef12ff35f8f996866f68fc6f5e7a9e3065f776
dr-strangelove-death <vlfedoseevad@gmail.com>
2016-07-15 16:25:34 +0300

    import changes

--- jobconstructor/__init__.py
+++ jobconstructor/__init__.py
@@ -1,10 +1,3 @@
 
-"""
-!!!
-It is ADVISABLE not to use/import this (jobconstructor) package in external modules (intended usage is for testing).
-Use/import src package instead (rename it to your liking and remove __main__.py module).
-For safety reasons functionality disabled.
-!!!
-"""
-
 from .src import JobConstructor
+from .src import exceptions
