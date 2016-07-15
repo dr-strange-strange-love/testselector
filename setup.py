b8de279e0ad208e80d3fab9ed071b011b9163780
dr-strangelove-death <vlfedoseevad@gmail.com>
2016-07-15 16:21:47 +0300

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(
+	name='jobconstructor',
+	version='1.0',
+	packages=['jobconstructor',],
+	license='GNU GENERAL PUBLIC LICENSE',
+	long_description=open('README.md').read(),
+)
